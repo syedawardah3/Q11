cbddcc3e043cdc9eba8923a04ff3092765d85426
syedawardah3 <148571080+syedawardah3@users.noreply.github.com>
2024-02-09 18:42:52 +0500

    Add files via upload

---
+++ Q11/Q11.ts
@@ -0,0 +1,9 @@
+// 11.Names: Store the names of a few of your friends in a array called names. Print each person’s name by accessing each element in the list, one at a time.
+
+// Array of friends' names
+let names: string[] = ['Alice', 'Bob', 'Charlie', 'David', 'Emily'];
+
+// Printing each person's name
+for (let name of names) {
+    console.log(name);
+}
